30bd9444891a631f31b6326ced6315fbcc900b2a
Tyler Oates <oatesrotc548@gmail.com>
2022-08-05 21:33:22 -0700

    Added remaining functions to dispay questions, answers, and depicitng so the user score is the time remaining

--- questions.js
+++ questions.js
@@ -1,5 +1,5 @@
 // Contains the arrays, question, question Number, and answers
-let questions = [
+var questions = [
     {
         question: "What year was Family Guy first Aired?",
         answer:"1999",
@@ -58,9 +58,9 @@ var score = 0;
 var timer = document.querySelector("#timer");
 var startTimer = document.querySelector("#startTimer");
 var quizBox = document.querySelector("#Quiz-box");
-var divQuestions = document.querySelector("#questions");
+var divQuestions = document.querySelector("#divQuestions");
 var divUl = document.createElement("ul");
-var penalty = 10;
+var timePenalty = 10;
 var Interval = 0;
 var secondsLeft = 100;
 
@@ -82,6 +82,7 @@ startTimer.addEventListener("click", function() {
     render(indexQuestions);
 })
 
+
 // displays questions to user 
 function render(indexQuestions) {
     divQuestions.innerHTML = "";
@@ -101,3 +102,97 @@ function render(indexQuestions) {
     })
 }
 
+// Function to depict on if the selected answer is correct or incorrect basied off the user choices and the correct answers
+function compareAnswers(event) {
+    var userElements = event.target;
+
+    if(userElements.matches("li")) {
+        var createDivId = document.createElement("div");
+        if (userElements.textContent == questions[indexQuestions].answer) {
+            score++;
+            createDivId.textContent = "Congrats! The correct answer is: " + questions[indexQuestions].answer;
+        } else {
+            secondsLeft = secondsLeft - timePenalty;
+            createDivId.textContent = "Wrong Answer! The correct answer is: " + questions[indexQuestions].answer;
+        }
+    }
+    // Determines what question the user is on
+    indexQuestions++;
+
+    if (indexQuestions >= questions.length) {
+        quizCompleted();
+        createDivId.textContent = "Finished!" + "You got " + score + "/" + questions.length + "Correct :)";
+    } else {
+        render(indexQuestions);
+    }
+    divQuestions.appendChild(createDivId);
+}
+
+function quizCompleted() {
+    divQuestions.innerHTML = "";
+    timer.innerHTML = "";
+
+    var header = document.createElement("h1");
+    header.setAttribute("id", "header");
+    header.textContent = "Congrats you have completed the quiz!";
+
+    divQuestions.appendChild(header);
+
+    var paragraph = document.createElement("p");
+    paragraph.setAttribute("id", "paragraph");
+
+    divQuestions.appendChild(paragraph);
+
+    if(secondsLeft >= 0) {
+        var remainingTime = secondsLeft;
+        var secondParagraph = document.createElement("p");
+        clearInterval(Interval);
+        paragraph.textContent = "Your completed score is:" + remainingTime;
+
+        divQuestions.appendChild(secondParagraph);
+    }
+
+    var label = document.createElement("label");
+    label.setAttribute("id", "label");
+    label.textContent = "Please enter your Initals: ";
+
+    divQuestions.appendChild(label);
+
+    var input = document.createElement("input");
+    input.setAttribute("type", "text");
+    input.setAttribute("id", "initals");
+    input.textContent = "";
+
+    divQuestions.appendChild(input);
+
+    var submitbutton = document.createElement("button");
+    submitbutton.setAttribute("type", "text");
+    submitbutton.setAttribute("id", "submit");
+    submitbutton.textContent ="Submit";
+
+    divQuestions.appendChild(submitbutton);
+
+    submitbutton.addEventListener("click", function() {
+        var userInitals = input.value;
+        if (userInitals === null) {
+            console.log("No user initals submitted!")
+        } else {
+            var totalScore = {
+                userInitals: userInitals,
+                score: remainingTime
+            }
+            console.log(totalScore);
+            var allUserScores = localStorage.getItem("allUserScores");
+            if (allUserScores === null) {
+                allUserScores = [];
+            } else {
+                allUserScores = JSON.parse(allUserScores);
+            }
+            allUserScores.push(totalScore);
+            var recentScore = JSON.stringify(allUserScores);
+            localStorage.setItem("allUserScores", recentScore);
+            window.location.replace("./highscoresheet.html");
+        }
+    });
+
+}
